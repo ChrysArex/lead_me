3adb250ad8a472818d39b051e3c09be578899de7
adoumasseo <adoumasseo@gmail.com>
2024-05-23 22:56:41 +0100

    [feat]: landing page

--- lead_me/models/Ecole.py
+++ lead_me/models/Ecole.py
@@ -1,27 +1,29 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from werkzeug.security import generate_password_hash
 from ..db import Base
+from Universites import Universite
 from uuid import uuid4
 
-class Serie(Base):
-    """Serie model to map the serie table
+class Ecole(Base):
+    """ecole model to map the serie table
     """
-    __tablename__ = 'serie'
-    id_serie = Column(String(128), primary_key=True, nullable=False)
+    __tablename__ = 'ecole'
+    id_ecole = Column(String(128), primary_key=True, nullable=False)
     nom = Column(String(10), nullable=False)
+    id_universite = Column(String(128), ForeignKey(Universite.Universite), nullable=False)
 
     def __init__(self, nom):
         """Initiate the model object with column values
         """
-        self.id_serie = str(uuid4())
+        self.id_ecole = str(uuid4())
         self.nom = nom
 
     def __str__(self):
         """Return a string representation of a serie
         """
-        return "Serie: {}".format(self.nom)
+        return "Ecole: {}".format(self.nom)
 
     def __repr__(self):
         """Return a string representation of a serie
         """
-        return "Serie: {}".format(self.nom)
+        return "Ecole: {}".format(self.nom)

91052f58c1a1b946b8abe80c20f3a42804fae19f
adoumasseo <adoumasseo@gmail.com>
2024-08-02 14:04:15 +0100

    [feat]: Gestion User Anonoyme

--- lead_me/enregistrement.py
+++ lead_me/enregistrement.py
@@ -4,12 +4,28 @@ from flask import (
         Blueprint, url_for, redirect, request, render_template
         )
 from flask_login import login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, ValidationError
 from .db import db
+from flask_login import login_user
 from .models.Matiere import Matiere
+from .models.User import User
 from .models.Serie import Serie
 from .models.Moyenne import Moyenne
 from .models.Filiere import Filiere
 notes_bp = Blueprint("notes", __name__, url_prefix="/notes")
+
+class CreateSerieForm(FlaskForm):
+    """
+    Cette classe se charge de créer le formulaire et ses champs
+    et de faire la validation des donnés
+    """
+    serie = SelectField("Série", choices=[], validators=[DataRequired(message="Veuillez sélectionner une Série.")])
+    submit = SubmitField('Soumettre')
+    def __init__(self, *args, **kwargs):
+        super(CreateSerieForm, self).__init__(*args, **kwargs)
+        self.serie.choices = [(u.id_serie, u.nom) for u in Serie.query.all()]
 
 @notes_bp.route("/traiter", methods=("GET", "POST"))
 @login_required
@@ -18,6 +34,23 @@ def traiter():
     matieres = serie.matiere
     return render_template("enregistrement.html", matieres=matieres)
 
+@notes_bp.route("/traitrement", methods=("GET", "POST"))
+def traiter_anonyme():
+    form = CreateSerieForm()
+    if form.validate_on_submit():
+        user = User (
+            nom = 'anonyme',
+            prenom= 'anonyme',
+            email = 'anonyme@gmail.com',
+            serie = form.serie.data,
+            matricule = 'ANONYME'
+        )
+        login_user(user)
+        serie = Serie.query.filter_by(nom=form.serie.data).first()
+        matieres = serie.matiere
+        return render_template("enregistrement.html", matieres=matieres)
+    return render_template("/frontend/form/create_anonyme_user.html", form=form)
+     
 @notes_bp.route("/resultat", methods=["POST"])
 def resultat():
     serie = Serie.query.filter_by(nom=current_user.serie).first()
